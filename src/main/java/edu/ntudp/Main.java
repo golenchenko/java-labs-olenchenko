6a4776181cbe9fabf350abcc8a5a76de3ce72766
Georgiy <olenchenko.h.m@nmu.one>
2024-01-22 18:08:39 +0200

    Main.java update

--- src/main/java/edu/ntudp/Main.java
+++ src/main/java/edu/ntudp/Main.java
@@ -2,6 +2,6 @@ package edu.ntudp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Мої вітання!");
     }
 }
