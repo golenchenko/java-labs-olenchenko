0e7ec9cb1e70436c066e8fc36274ded02a9af09f
Georgiy <olenchenko.h.m@nmu.one>
2024-03-09 15:49:28 +0200

    feat: Updated Human and Student classes (methods toString(), in Human.java added second constructor)

--- src/main/java/edu/ntudp/model/Student.java
+++ src/main/java/edu/ntudp/model/Student.java
@@ -1,13 +1,5 @@
 package edu.ntudp.model;
 
 public class Student extends Human {
-    private String groupName;
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
 
 }
