2939b29e7dc10977347f486ecb81e050b6a4f6dc
Georgiy <olenchenko.h.m@nmu.one>
2024-04-06 18:24:50 +0300

    refactor: Updated code

--- src/main/java/edu/ntudp/lab4/JsonManager.java
+++ src/main/java/edu/ntudp/lab4/JsonManager.java
@@ -2,14 +2,8 @@ package edu.ntudp.lab4;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.stream.JsonReader;
-import edu.ntudp.lab4.model.Group;
-import edu.ntudp.lab4.model.StructureUnit;
-import edu.ntudp.lab4.model.University;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class JsonManager{
     public JsonManager() {
